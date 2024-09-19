18bc849290b0dda5d30bb648b1edbc481c76ae18
Sa1shan <grigalex06@gmail.com>
2024-09-19 04:03:19 +0300

    Feat: Enemy AI

--- ShadowPixel/Assets/Script/EnemyAI.cs
+++ ShadowPixel/Assets/Script/EnemyAI.cs
@@ -1,8 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    public GameObject player;
+    public Collider2D Collider;
     
+    private NavMeshAgent _navMeshAgent;
+    
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.updateRotation = false;
+        _navMeshAgent.updateUpAxis = false;
+    }
+    
+    private void Update()
+    {
+        Chasting();
+    }
+    private void Chasting()
+    {
+        { 
+            _navMeshAgent.SetDestination(player.transform.position);
+        }
+    }
+
+   
 }
